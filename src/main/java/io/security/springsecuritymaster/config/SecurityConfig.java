4eebfde30e6b17de3a3b3557e638d085ae20c4b6
hyeOOO <hyewon5342@nate.com>
2024-07-24 20:48:38 +0900

    [Feat] 로그인 페이지 생성

--- src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/config/SecurityConfig.java
@@ -22,10 +22,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll() //정적 자원 접근 허용
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form.loginPage("/login").permitAll())
         ;
 
         return http.build();
